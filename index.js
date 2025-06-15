e4658681a762459c8dd51a14f3caaac382ed66cb
Roy Koigu <roykoigu@protonmail.com>
2025-06-15 16:38:10 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`
+}
+saturdayFun();
+//console.log(saturdayFun())
+
+function mondayWork(action = 'go to the office'){
+    return `This Monday, I will ${action}.`
+}
+mondayWork("work from home");
+//console.log(mondayWork("take a shit"))
+
+function wrapAdjective(flair = "*"){
+    return function(adjective){
+        return `You are ${flair}${adjective}${flair}!`}
+    }
+
+    console.log(wrapAdjective("*")("the slayer"))
+
